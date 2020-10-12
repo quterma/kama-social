746ecb2cd40ec9fb7a2f852c358c74f5c539816a
Quterma <wolf921@mail.ru>
2020-10-12 12:58:28 +0300

    85. add useEffect hook into ProfileStatus

--- src/Components/Profile/ProfileInfo/ProfileStatus/ProfileStatusWithHooks.jsx
+++ src/Components/Profile/ProfileInfo/ProfileStatus/ProfileStatusWithHooks.jsx
@@ -1,18 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./ProfileStatus.module.css";
 
 const ProfileStatusWithHooks = props => {
-
+	// hook for edit mode state and it's toggling
 	const [editMode, setEditMode] = useState(false);
+
+	// hook for status editing
 	const [status, setStatus] = useState(props.status);
+
+	// f() for activating edit mode (toggling editMode state in true)
 	const activateEditMode = () => setEditMode(true);
+	
+	// f() for deactivating edit mode (toggling editMode state in false and status 'dispatching' into store)
 	const deactivateEditMode = () => {
 		setEditMode(false);
 		props.updateStatus(status);
 	}
+
+	// f() for parsing status value 'from event' to status
 	const onStatusChange = (event) => {
 		setStatus(event.currentTarget.value);
 	}
+
+	// hook subscribing for props.status
+	useEffect(() => { setStatus(props.status) }, [props.status]);
+	
 		return (
 			<div>
 				{ !editMode &&
